e15fa9906e403990ff93523d825b51261c5046ac
Matt Smithies <info@mattsmithies.co.uk>
2021-02-07 17:38:51 +0000

    Update depositTokenPoolHandler.js

--- app/handler/pool/depositTokenPoolHandler.js
+++ app/handler/pool/depositTokenPoolHandler.js
@@ -1,78 +1,87 @@
-import createPoolRequest from "app/validators/pool/createPoolRequest"
+import depositPoolRequest from "app/validators/pool/depositPoolRequest"
 import Response from "app/response"
 import TokenData from "app/database/tokens";
-import PoolData from "../../database/pool";
+import prepare from "app/utils/prepare";
+import withRegisteredAccount from "app/middleware/withRegisteredAccount";
+import DepositTokenService from "app/services/hashgraph/pool/depositTokenService";
 
 async function DepositTokenPoolHandler(req, res) {
-  const validationErrors = createPoolRequest(req.body)
+
+  const { body, authorisationAccount } = req
+  const validationErrors = depositPoolRequest(body)
 
   if (validationErrors) {
     return Response.unprocessibleEntity(res, validationErrors)
   }
 
-  const {
-    token_id
-  } = req.body
-
+  const { token_id, amount } = body
   const token = await TokenData.find(token_id)
 
+  // 1. The token needs to exist
   if (!token) {
-    return Response.badRequest(res, { error: `Token with token_id: ${token_id} does not exist` } )
+    return Response.unprocessibleEntity(res, { error: `Token with token_id: ${token_id} does not exist` } )
   }
 
-  const poolExists = await PoolData.find(token.id)
+  const pool = token?.Pool?.[0]
 
-  if (!poolExists) {
-    return Response.badRequest(res, { error:  `Pool with token_id: ${token_id} already exists` })
+  // 2. The pool needs to exist
+  if (!pool) {
+    return Response.unprocessibleEntity(res, { error:  `Pool with token_id: ${token_id} does not exist` })
   }
 
-  /**
-   * Depositing a token
-   *
-   * 1. The token needs to exist
-   * 2. The pool needs to exist
-   * 3. The token and amount needs to exist in users holdings
-   * 4. The token needs to be transferred to the pool
-   * 5. A deposit reward pool needs to be created with token preferences
-   */
+  const { hashgraphClient } = req.context
 
+  const service = new DepositTokenService({
+    hashgraphClient,
+    pool,
+    token,
+    authorisationAccount
+  })
 
+  const currentRplPool = await service.checkForRewardLiquidityPoolExists()
+  
+  if (currentRplPool) {
+    return Response.unprocessibleEntity(res, {error: `Reward Liquidity Pool for user: ${authorisationAccount.hedera_id} for token ${token_id} already exists`})
+  }
 
-  //
-  // const poolExists = await PoolData.find(token.id)
-  //
-  // if (poolExists) {
-  //   return Response.badRequest(res, { error:  `Pool with token_id: ${token_id} already exists` })
-  // }
-  //
-  // const { hashgraphClient } = req.context
-  // const accountWithKeys = await hashgraphClient._createNewAccount()
-  //
-  // const encrypted = await Sodium.encrypt({
-  //   message: accountWithKeys.privateKey,
-  //   signature: Config.privateKey
-  // })
-  //
-  // const account = await AccountData.createHederaAccount({
-  //   hedera_id: accountWithKeys.accountId,
-  //   enc_skey: encrypted,
-  //   public_key: accountWithKeys.publicKey
-  // })
-  //
-  // await PoolData.createPool({
-  //   name: `${token.symbol} Pool`,
-  //   amount: 0,
-  //   tokenId: token.id,
-  //   accountId: account.id
-  // })
-  //
-  // await hashgraphClient.associateToAccount({
-  //   privateKey: accountWithKeys.privateKey,
-  //   tokenIds: [ token.token_id ],
-  //   accountId: account.hedera_id
-  // })
+  const userTokenHolding = await service.getUserTokenHolding()
 
+  const proposedAmount = parseFloat(amount) * 10 ** userTokenHolding.token.decimals
+  const tokenHodlStack = userTokenHolding.amount
+
+  // 3. The token and amount needs to exist in users holdings
+  if (proposedAmount > tokenHodlStack) {
+    return Response.unprocessibleEntity(res, { error:  `You cannot deposit more tokens then you own into the pool` })
+  }
+
+  await service.transferTokensToPool(proposedAmount)
+  await service.updatePoolValue(proposedAmount)
+
+  const { proxyLiquidityPool, account } = await service.createNewRewardPoolAccount()
+
+  const stored = await service.createLpToken({
+    proxyLiquidityPool,
+    account,
+    amount,
+    proposedAmount
+  })
+
+  await service.createRewardLiquidityPool({
+    account,
+    proposedAmount
+  })
+
+  await service.transferLpTokenToDepositee({
+    proposedAmount,
+    stored,
+    proxyLiquidityPool,
+    account
+  })
+
+  // Update this
   Response.json(res, "ok")
 }
 
-export default DepositTokenPoolHandler
+export default prepare(
+  withRegisteredAccount
+)(DepositTokenPoolHandler)
