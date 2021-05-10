0f52a833b7ed7a5a65c7af3a3c24218287ee05da
vishal <vishal.dharmawat@gmail.com>
2021-05-10 23:18:43 +0530

    wrapped in try-catch and variable naming updated.

--- app/middleware/checkSignature.js
+++ app/middleware/checkSignature.js
@@ -3,10 +3,14 @@ import hmac from '../utils/hmac'
 
 function checkSignature(handler) {
 	return async (req, res) => {
-        const { "x-signature" : reqSignature } = req.headers;
-        const payloadBody = JSON.stringify(req.body);
-        let isValid = hmac.validateSignature(payloadBody, reqSignature)
-        if(!isValid) {
+        try {
+            const { "x-signature" : requsetSignature } = req.headers;
+            const payloadAsString = JSON.stringify(req.body);
+            let isValid = hmac.validateSignature(payloadAsString, requsetSignature)
+            if(!isValid) {
+                return Response.badRequest(res, "Bad Request")
+            }
+        } catch (error) {
             return Response.badRequest(res, "Bad Request")
         }
         return handler(req, res)
